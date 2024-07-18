9b72ccdc184ab82007e946a45deff76c7c370335
Mithunmsrd <mithunanayadi@gmail.com>
2024-07-18 07:41:10 +0530

    project

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,27 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import {createBrowserRouter,createRoutesFromElements,RouterProvider,Route,} from "react-router-dom";
+import Home from './components/Home'
+import Userlogin from './pages/Userlogin';
+import UserRegister from './pages/UserRegister';
+import Restaurant from "./pages/Restaurant";
+import Food1 from "./pages/Food1";
+import Food2 from "./pages/Food2";
+import Food3 from "./pages/Food3";
+import Payment from "./pages/Payment";
+import AdminLogin from "./pages/AdminLogin";
+import AdminRegister from "./pages/AdminRegister";
+import AddHotel from "./pages/AddHotel";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ FRONTEND/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav class="bg-gray-800 p-4">
+        
+
+          
+        <div class="hidden md:block">
+            <a href="#" class="text-gray-300 hover:text-white px-4">Home</a>
+            <a href="restaurant.html" class="text-gray-300 hover:text-white px-4">Restaurant</a>
+            <a href="#" class="text-gray-300 hover:text-white px-4">Contact</a>
+        </div>
+
+
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
+
